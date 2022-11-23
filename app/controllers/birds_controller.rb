cf03f6ed9e8683eb851fcd5e6f5dd3f15366f96d
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-11-23 16:41:44 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+ wrap_parameters format: []
 
   # GET /birds
   def index
@@ -16,4 +17,14 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  private
+  
+  def bird_params
+   params.permit(:name, :species)
+  end 
 end
